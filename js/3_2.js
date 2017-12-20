d5e371908035f48d8d6f9bbd7fde6e4807310004
micro-wyx <13011291594@163.com>
2017-12-20 19:45:48 +0800

    Initial commit

---
+++ js/3_2.js
@@ -0,0 +1,17 @@
+function getChildren1(parent) {
+    console.log(parent.nodeType!=3?parent.nodeName:parent.nodeValue);
+    var children=parent.childNodes;
+    for(var i=0;i<children.length;i++){
+        // console.log(child.nodeType!=3?child.nodeName:child.nodeValue);
+        getChildren1(children[i]);
+    }
+
+}
+function getChildren2(parent) {
+    var iterator=document.createNodeIterator(parent,NodeIterator.SHOW_ALL,null,false);
+var node;
+    while((node=iterator.nextNode())!=null){
+        console.log(node.nodeType!=3?node.nodeName:node.nodeValue);
+    }
+}
+getChildren2(document.body);
